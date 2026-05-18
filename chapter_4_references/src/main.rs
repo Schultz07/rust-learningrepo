f8d242d53c2d59d414952ac626cc9db3d861024d
Schultz07 <peterschultz2607@proton.me>
2026-05-18 02:43:26 -0300

    wip: references

---
+++ chapter_4_references/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let mut s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+    println!("The length of '{s1}' is {len}.");
+
+    change(&mut s1);
+    let len = calculate_length(&s1);
+    println!("The length of '{s1}' is {len}.");
+
+    let mut s = String::from("hello");
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("{r1} and {r2}");
+
+    let r3 = &mut s;
+    println!("{r3}");
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
