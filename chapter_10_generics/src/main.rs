350750ec20555b6bdc9cccbbe6da123ee154d76d
Schultz07 <peterschultz2607@proton.me>
2026-05-31 02:22:54 -0300

    wip: generics

---
+++ chapter_10_generics/src/main.rs
@@ -0,0 +1,58 @@
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+
+    println!("The largest number is {result}");
+
+    let char_list = vec!['t', 'd', 'k', 'l', 'n', 'm', 'a'];
+
+    let result: &char = largest(&char_list);
+
+    println!("The largest char is {result}");
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
+
+fn largest<T>(list: &Vec<T>) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
